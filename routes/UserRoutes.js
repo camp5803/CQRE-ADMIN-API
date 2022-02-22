111a7de0b65a3f8874a188d90f6c74d5f8d01578
Jiyong Jung <hickerd@sch.ac.kr>
2022-02-23 04:32:58 +0900

    read와 readAll이 반대로 동작하던 오류 수정

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,8 +4,8 @@ const { read, readAll, update, destroy } = require('../controllers/UserControlle
 
 const router = express.Router();
 
-router.get('/', wrap(read));
-router.get('/:uid', wrap(readAll));
+router.get('/', wrap(readAll));
+router.get('/:uid', wrap(read));
 router.patch('/:uid', wrap(update));
 router.delete('/:uid', wrap(destroy));
 
