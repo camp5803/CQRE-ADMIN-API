83ed8bf79e863aae82d5d8cffe9d955326b08dea
Jiyong Jung <hickerd@sch.ac.kr>
2022-02-23 04:58:14 +0900

    도서 반납 및 연체자 처리 기능 추가

---
+++ controllers/RentalController.js
@@ -0,0 +1,24 @@
+const { Rental, Delinquent } = require('../models')
+
+const returnBook = async (req, res) => {
+    const data = await Rental.findOne({
+        where: { user_id: req.params.uid },
+        raw: true
+    });
+    const now = new Date();
+
+    if (now > data.return_at) {
+        Delinquent.create({
+            user_id: req.params.uid,
+            expiry_date: new Date(now.getTime() + (now.getTime() - data.return_at.getTime()))
+        });
+    }
+
+    Rental.destroy({
+        where: { user_id: req.params.uid }
+    });
+
+    return res.json({ success: true });
+}
+
+module.exports = returnBook;
--- routes/RentalRoutes.js
+++ routes/RentalRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const wrap = require('../wrapper');
 const { read } = require('../controllers/DelinquentController');
+const returnBook = require('../controllers/RentalController');
 
 const router = express.Router();
 
 router.get('/delinquency', read);
+router.delete('/delinquency/:uid', returnBook);
 
 module.exports = router;
