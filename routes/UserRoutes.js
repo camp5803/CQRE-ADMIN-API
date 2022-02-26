d180eac6d8416b5c8bef1a72e57efe8fecf3096c
Jiyong Jung <hickerd@sch.ac.kr>
2022-02-26 18:47:33 +0900

    인증 미들웨어 추가

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const wrap = require('../wrapper');
 const { read, readAll, update, destroy } = require('../controllers/UserController');
+const { isAuthenticated } = require('../controllers/AuthController');
 
 const router = express.Router();
+
+router.use(isAuthenticated);
 
 router.get('/', wrap(readAll));
 router.get('/:uid', wrap(read));
